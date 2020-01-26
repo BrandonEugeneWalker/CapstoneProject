fdb78bf4f7da1850038281123ddfc398fb97dea3
Brandon Walker <brandoneugenewalker@outlook.com>
2020-01-26 15:58:45 -0500

    Initial Commit for desktop files.

---
+++ code/desktop/Capstone Desktop Tests/Model Tests/LibraryItemTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Capstone_Desktop_Tests.Model_Tests
+{
+    class LibraryItemTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
