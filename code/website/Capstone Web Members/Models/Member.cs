5292229f41d774a4246f8f8dac15698ca02febb0
TheJeselnik <dannyjeselnik@gmail.com>
2020-01-27 14:29:17 -0500

    adding member class base

---
+++ code/website/Capstone Web Members/Models/Member.cs
@@ -0,0 +1,56 @@
+﻿
+namespace Capstone_Web_Members.Models
+{
+    /// <summary>
+    /// Defines a member logging into the website to borrow media
+    /// </summary>
+    public class Member
+    {
+
+        /// <summary>
+        /// Gets or sets the member identifier.
+        /// </summary>
+        /// <value>
+        /// The member identifier.
+        /// </value>
+        public int MemberId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public string LastName { get; set; }
+
+
+        public Member()
+        {
+
+        }
+    }
+}
