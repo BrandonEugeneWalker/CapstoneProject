227d51b4b8512fd23ffacd90a40a9b068b07eaaa
Caleb Bell <cbell12@my.westga.edu>
2020-01-27 17:20:04 -0500

    Created Warehouse website with empty pages.

---
+++ code/website/Capstone Web Warehouse/Capstone Web Warehouse/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone_Web_Warehouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ManageItems()
+        {
+            ViewBag.Message = "Item management page.";
+
+            return View();
+        }
+
+
+        public ActionResult ManageEmployee()
+        {
+            ViewBag.Message = "Employee management page.";
+
+            return View();
+        }
+
+    }
+}
