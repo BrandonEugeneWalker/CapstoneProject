ad2ca65108ff5607a68ad5d671f381aead8c32da
Caleb Bell <cbell12@my.westga.edu>
2020-01-28 03:01:00 -0500

    Added media library, displays media, requires login to view.

--- code/website/Capstone Web Members/Controllers/HomeController.cs
+++ code/website/Capstone Web Members/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Capstone_Web_Members.Models;
 
 namespace Capstone_Web_Members.Controllers
 {
     public class HomeController : Controller
     {
+        private DefaultEntity data = new DefaultEntity();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,12 @@ namespace Capstone_Web_Members.Controllers
             ViewBag.Message = "Contact Information:";
 
             return View();
+        }
+
+        [Authorize]
+        public ActionResult MediaLibrary()
+        {
+            return View(this.data.Products.ToList());
         }
     }
 }
