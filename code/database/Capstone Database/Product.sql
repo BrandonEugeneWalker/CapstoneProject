4baf3c0a017cd085423096f56ae3562f80cfb08c
Brandon <bwalke18@my.westga.edu>
2020-02-08 14:05:00 -0500

    Removed unused database project.
    This is so we can replace it with a new solution that handles all database items using a dbcontext.

--- code/database/Capstone Database/Product.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Product]
-(
-	[productId] INT NOT NULL PRIMARY KEY, 
-    [name] VARCHAR(50) NOT NULL, 
-    [description] VARCHAR(300) NOT NULL, 
-    [type] VARCHAR(50) NOT NULL
-)
--- code/database/Capstone Database/Rental.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Rental]
-(
-	[rentalId] INT NOT NULL PRIMARY KEY, 
-    [stockId] INT NOT NULL, 
-    [memberId] INT NOT NULL, 
-    CONSTRAINT [FK_Rental_Stock] FOREIGN KEY ([stockId]) REFERENCES [Stock]([stockId]), 
-    CONSTRAINT [FK_Rental_Member] FOREIGN KEY ([memberId]) REFERENCES [Member]([memberId])
-)
