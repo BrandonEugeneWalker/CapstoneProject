ac23b11855e3a510064fdef74d10219ea8792fd6
Brandon Walker <brandoneugenewalker@outlook.com>
2020-02-12 00:29:53 -0500

    Removed tests for desktop because it used the wrong framework (NUnit is .net core).
    Added centeralized unit test solution, hopefully we can move all our tests there.

---
+++ code/tests/Capstone Unit Tests/desktop/controller/AddEmployeeFormControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone_Unit_Tests.desktop.controller
+{
+    [TestClass]
+    public class AddEmployeeFormControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ code/tests/Capstone Unit Tests/desktop/controller/LoginFormControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone_Unit_Tests.desktop.controller
+{
+    [TestClass]
+    public class LoginFormControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
