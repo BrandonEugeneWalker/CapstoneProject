7d869dd2c34491b7bd27c4909f03c5e4b975fef5
Caleb Bell <cbell12@my.westga.edu>
2020-02-12 02:10:45 -0500

    Added unit testing for database dbcontext

---
+++ code/database/Capstone Database Unit Test/OnlineEntitiesTests.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone_Database.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Toolchains.Roslyn;
+using FizzWare.NBuilder;
+using Moq;
+
+namespace Capstone_Database.Model.Tests
+{
+    [TestClass()]
+    public class OnlineEntitiesTests
+    {
+        [TestMethod()]
+        public void OnlineEntitiesTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void createMemberOrderTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void retrieveAllProductsTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void retrieveAvailableProductsTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void retrieveProductsWaitingReturnTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void retrieveProductsWaitingReturnTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void retrieveProductsWaitingShipmentTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void retrieveProductsWaitingShipmentTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void retrieveRentedCountTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void searchProductsNameTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void searchProductsNameTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void searchProductsNameTypeTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void searchProductsNameTypeTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void searchProductsTypeTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void searchProductsTypeTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void updateMemberReturnTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void updateRentalShippedTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void updateReturnProcessedTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void selectEmployeeByIdAndPasswordTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void selectEmployeeByIdAndPasswordTest1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void insertEmployeeTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
