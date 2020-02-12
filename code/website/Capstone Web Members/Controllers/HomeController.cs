a2693f3508a0ee720dc37606f18ec18c09374bd5
TheJeselnik <dannyjeselnik@gmail.com>
2020-02-11 21:25:12 -0500

    Adding buttons for order

--- code/website/Capstone Web Members/Controllers/HomeController.cs
+++ code/website/Capstone Web Members/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
-using Capstone_Database;
 using Capstone_Database.Model;
-using Capstone_Web_Members.Models;
 
 namespace Capstone_Web_Members.Controllers
 {
