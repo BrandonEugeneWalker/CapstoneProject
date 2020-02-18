a12881af9da89873be04866c9d3d3e38586191b7
TheJeselnik <dannyjeselnik@gmail.com>
2020-02-18 16:53:51 -0500

    Further test set up for Member Web
    -Added setup stage for creating products in a list
    -added documentation for all public properties and functions in HomeControllerTest.cs . Each test method briefly describes its purpose, class description added

--- code/tests/Capstone Unit Tests/web/members/HomeControllerTest.cs
+++ code/tests/Capstone Unit Tests/web/members/HomeControllerTest.cs
@@ -1,12 +1,48 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Capstone_Database.Model;
 using Capstone_Web_Members.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Capstone_Unit_Tests.web.members
 {
+    /// <summary>
+    /// Tests the HomeController behavior
+    /// </summary>
     [TestClass]
     public class HomeControllerTest
     {
+
+        /// Setup for Test Products
+        private static IEnumerable<Product> GetTestProducts()
+        {
+            var productA = new Product
+            {
+                productId = 1,
+                name = "The Hobbit",
+                description = "One small boi goes on an adventure",
+                type = "Book",
+                category = "Adventure"
+            };
+            var productB = new Product
+            {
+                productId = 1,
+                name = "Fellowship of the Ring",
+                description = "The fate of Middle-earth hangs in the balance as Frodo and " +
+                              "eight companions who form the Fellowship of the Ring begin their " +
+                              "journey to Mount Doom - Movie Adaptation",
+                type = "Movie",
+                category = "Fantasy"
+            };
+
+            var testProducts = new List<Product>() { productA, productB };
+
+            return testProducts;
+        }
+
+        /// <summary>
+        /// Tests the controller behavior for the member web Index
+        /// </summary>
         [TestMethod]
         public void Index()
         {
@@ -20,6 +56,9 @@ namespace Capstone_Unit_Tests.web.members
             Assert.IsNotNull(result);
         }
 
+        /// <summary>
+        /// Tests the controller behavior for the member web About page
+        /// </summary>
         [TestMethod]
         public void About()
         {
@@ -34,6 +73,9 @@ namespace Capstone_Unit_Tests.web.members
             Assert.IsNotNull(result);
         }
 
+        /// <summary>
+        /// Tests the controller behavior for the member web Contact info page
+        /// </summary>
         [TestMethod]
         public void Contact()
         {
@@ -48,6 +90,9 @@ namespace Capstone_Unit_Tests.web.members
             Assert.IsNotNull(result);
         }
 
+        /// <summary>
+        /// Tests the controller behavior for the Media Library
+        /// </summary>
         [TestMethod]
         public void MediaLibrary()
         {
@@ -57,12 +102,16 @@ namespace Capstone_Unit_Tests.web.members
             // Act
             var result = controller.MediaLibrary() as ViewResult;
             var model = result.Model;
-
+            var testProducts = GetTestProducts();
 
             // Assert
             Assert.IsNotNull(result);
         }
 
+        /// <summary>
+        /// Tests the controller behavior for the Media Library when ordering a product
+        /// </summary>
+        /// <param name="id">The identifier.</param>
         [TestMethod]
         public void OrderProduct(int id)
         {
