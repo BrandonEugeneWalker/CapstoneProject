a7b9ccbbefe9da27d6e9ab607b90e2972a5180c5
TheJeselnik <dannyjeselnik@gmail.com>
2020-02-18 16:30:21 -0500

    member web tests working in test project
    -Added all previous Member Web tests to the Test Project, verified they have the same results and scope
    -Removed old Member Web Test Project.

--- code/tests/Capstone Unit Tests/web/members/HomeControllerTest.cs
+++ code/tests/Capstone Unit Tests/web/members/HomeControllerTest.cs
@@ -1,12 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Web.Mvc;
+using Capstone_Web_Members.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Capstone_Unit_Tests.web.members
 {
-    class HomeControllerTest
+    [TestClass]
+    public class HomeControllerTest
     {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("About Us", result.ViewBag.Message);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Contact Information:", result.ViewBag.Message);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void MediaLibrary()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.MediaLibrary() as ViewResult;
+            var model = result.Model;
+
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void OrderProduct(int id)
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.MediaLibrary() as ViewResult;
+            var model = result.Model;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
