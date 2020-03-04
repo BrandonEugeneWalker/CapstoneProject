1445bc9959e273c1d2c2dcbf2f5f10580d6d78e1
Caleb Bell <cbell12@my.westga.edu>
2020-03-03 20:01:18 -0500

    Added web warehouse to update itemship table and display employee history.
    -Updating status on manage items page now adds it to itemship table
    -In RentalsController modified updatestatus to create a new itemship object based on the employee logged in and the item rental selected.
    --For now item condition is always set to good.
    -Changed view on employee Detail page to show a table of all the rentals that the employee has either shipped out or received.

--- code/website/Capstone Web Warehouse/Capstone Web Warehouse/Controllers/RentalsController.cs
+++ code/website/Capstone Web Warehouse/Capstone Web Warehouse/Controllers/RentalsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
@@ -33,50 +34,51 @@ namespace Capstone_Web_Warehouse.Controllers
             var itemRentals = database.ItemRentals.Include(i => i.Member).Include(i => i.Stock).Where(i => i.status.Equals("WaitingReturn") || i.status.Equals("WaitingShipment"));
             return View(itemRentals.ToList());
         }
-/*
-        /// <summary>
-        ///     <para>
-        ///         Returns update page for manage rentals.
-        ///     </para>
-        ///     <precondition>The selected rental ID != null && must exist in the database.</precondition>
-        /// </summary>
-        /// <param name="id">  The selected itemRental ID for the rental to be modified.</param>
-        /// <returns>The item rental update page view with selected rental ID.</returns>
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-
-            var itemRental = database.ItemRentals.Find(id);
-            return itemRental == null ? (ActionResult) HttpNotFound() : View(itemRental);
-        }
-
-        /// <summary>
-        ///     Edits the specified item rental.
-        ///     Used for update button.
-        /// </summary>
-        /// <param name="itemRental">The itemRental to be updated.</param>
-        /// <returns>The item rentals index page if input valid. || The item rentals update page with entered information.</returns>
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "itemRentalId,stockId,memberId,status")]
-            ItemRental itemRental)
-        {
-            if (ModelState.IsValid)
-            {
-                database.Entry(itemRental).State = EntityState.Modified;
-                database.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(itemRental);
-        }*/
 
         /*
-         * Updates rental status to either rented or returned.
-         */
+                /// <summary>
+                ///     <para>
+                ///         Returns update page for manage rentals.
+                ///     </para>
+                ///     <precondition>The selected rental ID != null && must exist in the database.</precondition>
+                /// </summary>
+                /// <param name="id">  The selected itemRental ID for the rental to be modified.</param>
+                /// <returns>The item rental update page view with selected rental ID.</returns>
+                public ActionResult Edit(int? id)
+                {
+                    if (id == null)
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    }
+
+                    var itemRental = database.ItemRentals.Find(id);
+                    return itemRental == null ? (ActionResult) HttpNotFound() : View(itemRental);
+                }
+
+                /// <summary>
+                ///     Edits the specified item rental.
+                ///     Used for update button.
+                /// </summary>
+                /// <param name="itemRental">The itemRental to be updated.</param>
+                /// <returns>The item rentals index page if input valid. || The item rentals update page with entered information.</returns>
+                [HttpPost]
+                [ValidateAntiForgeryToken]
+                public ActionResult Edit([Bind(Include = "itemRentalId,stockId,memberId,status")]
+                    ItemRental itemRental)
+                {
+                    if (ModelState.IsValid)
+                    {
+                        database.Entry(itemRental).State = EntityState.Modified;
+                        database.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+
+                    return View(itemRental);
+                }*/
+
+        /// <summary>Updates the status of rental item.</summary>
+        /// <param name="id">The id of the rental item.</param>
+        /// <returns>The index page refreshed if found || error page if bad id.</returns>
         public ActionResult UpdateStatus(int? id)
         {
             if (id == null)
@@ -84,19 +86,53 @@ namespace Capstone_Web_Warehouse.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
+            var employee = Session["Employee"] as Employee;
             var rental = database.ItemRentals.Find(id);
+
+            if (rental == null || employee == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
             if (rental.status.Equals("WaitingShipment"))
             {
-                rental.status = "Rented";
+                rental.status = "WaitingReturn";
+                var itemShip = new ItemShip()
+                {
+                    employeeId = employee.employeeId,
+                    itemRentalId = rental.itemRentalId,
+                    itemCondition = "Good",
+                    itemDescription = "Item was shipped.",
+                    shipDateTime = DateTime.Now
+                };
+
+                database.ItemShips.Add(itemShip);
                 database.SaveChanges();
-            }else if (rental.status.Equals("WaitingReturn"))
+                return Redirect(HttpContext.Request.UrlReferrer?.AbsoluteUri);
+            }
+
+            if (rental.status.Equals("WaitingReturn"))
             {
                 rental.status = "Returned";
+                var itemShip = new ItemShip()
+                {
+                    employeeId = employee.employeeId,
+                    itemRentalId = rental.itemRentalId,
+                    itemCondition = "Good",
+                    itemDescription = "Item was received.",
+                    shipDateTime = DateTime.Now
+                };
+
+                database.ItemShips.Add(itemShip);
                 database.SaveChanges();
+                return Redirect(HttpContext.Request.UrlReferrer?.AbsoluteUri);
             }
+
             return Redirect(HttpContext.Request.UrlReferrer?.AbsoluteUri);
         }
 
+        /// <summary>Releases unmanaged resources and optionally releases managed resources.</summary>
+        /// <param name="disposing">true to release both managed and unmanaged resources; false to release only unmanaged resources.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing) database.Dispose();
