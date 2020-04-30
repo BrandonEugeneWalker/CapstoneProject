53f0258327fa8116d0ec4f85d30fd4e39a51b7d7
Daniel Jeselnik <djeseln1@my.westga.edu>
2020-04-30 15:49:40 -0400

    Added Required for Member name field in registration

--- code/database/Capstone Database/Model/Member.cs
+++ code/database/Capstone Database/Model/Member.cs
@@ -25,6 +25,7 @@ namespace Capstone_Database.Model
         public int memberId { get; set; }
         [Required(ErrorMessage = "Required")]
         public string username { get; set; }
+        [Required]
         public string name { get; set; }
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [Display(Name = "Password")]
